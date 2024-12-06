b2961bf27a4bfd0eabade0651b7fdb64d658a781
parmarraj999 <rajraja5122@gmail.com>
2024-12-06 13:32:41 +0530

    comit

--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -4,20 +4,13 @@ const AppointmentSchema = require("../models/appointment")
 
 // for booking apoointment
 router.post('/bookDoctor/:id',async(req,res)=>{
-    const { id } = req.params;
-    const { patientName, patientNumber, patientAge, appointmentDate, appointmentTime, status} = req.body();
-    try {
-        const newAppointment = new AppointmentSchema({
-            patientName,
-            patientNumber,
-            patientAge,
-            appointmentDate,
-            appointmentTime,
-            status,
-        })
-        await newAppointment.save();
-        res.status(201).json(newAppointment);
-    } catch (error) {
-        res.status(400).json({ error: err.message });
-    }
+   
+})
+
+router.get("/searchDoctor", async(req,res)=>{
+
+})
+
+router.post("/medicineRequest",async(req,res)=>{
+    
 })
